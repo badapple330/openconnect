75a7dbfc7aff962990263b6ddee6778e20671307
Yamada <akiautumndew@gmail.com>
2016-05-26 16:44:00 +0900

    ファイルとクラス作成しました 山田

---
+++ src/jp/co/internous/dao/KintaiKessekiDAO.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jp.co.internous.dao;
+
+/**
+ * @author internous
+ *
+ */
+public class KintaiKessekiDAO {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
