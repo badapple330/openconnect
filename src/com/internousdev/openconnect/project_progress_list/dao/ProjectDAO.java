4f3be936f8b31750804fb4d3d3794975a8374944
tatuhumi ito <forest-of-earth@docomo.ne.jp>
2016-08-31 11:42:41 +0900

    プロジェクト進捗状況

---
+++ src/com/internousdev/openconnect/project_progress_list/dao/ProjectDAO.java
@@ -0,0 +1,51 @@
+package com.internousdev.openconnect.project_progress_list.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.internousdev.openconnect.project_progress_list.dto.GetAddressDTO;
+import com.internousdev.util.DBConnector;
+
+public class ProjectDAO {
+
+	private List<GetAddressDTO>projectList=new ArrayList<GetAddressDTO>();
+
+	public List<GetAddressDTO> select() {
+		DBConnector db = new DBConnector("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/", "openconnect", "root","mysql");
+		Connection con = db.getConnection();
+
+		try {
+			String sql="SELECT*FROM project_progress_list ";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				GetAddressDTO dto = new GetAddressDTO();
+				dto.setProjectId(rs.getInt("project_id"));
+				dto.setProjectDay(rs.getString("project_day"));
+				dto.setProject(rs.getString("project"));
+				dto.setProjectPlan(rs.getString("project_plan"));
+				dto.setProjectResult(rs.getString("project_result"));
+				dto.setOther(rs.getString("other"));
+
+				projectList.add(dto);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return projectList;
+	}
+
+
+
+}
---
+++ src/com/internousdev/openconnect/project_progress_list/dao/ProjectNewDAO.java
@@ -0,0 +1,52 @@
+/**
+ *
+ */
+package com.internousdev.openconnect.project_progress_list.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+
+import com.internousdev.util.DBConnector;
+
+
+public class ProjectNewDAO {
+
+
+	public int insert(String projectDay,String project,String projectPlan,String projectResult,String other){
+
+		int count = 0;
+
+		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
+		projectDay = sdf.format(System.currentTimeMillis());
+
+
+		DBConnector db = new DBConnector("com.mysql.jdbc.Driver","jdbc:mysql://localhost/","openconnect","root","mysql");
+		Connection conn = db.getConnection();
+		String sql = "INSERT INTO project_progress_list(project_day,project,project_plan,project_result,other)VALUES(?, ?, ?, ? ,?)";
+		try{
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1,projectDay);
+			ps.setString(2,project);
+			ps.setString(3,projectPlan);
+			ps.setString(4,projectResult);
+			ps.setString(5,other);
+
+			count = ps.executeUpdate();
+
+
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally {
+			try{
+				conn.close();
+			}catch(SQLException e){
+				e.printStackTrace();
+			}
+		}
+		return count;
+
+	}
+
+}
