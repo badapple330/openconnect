83de66e805368ff8c529ca63b821d16bb761f61c
tatuhumi ito <forest-of-earth@docomo.ne.jp>
2016-09-01 10:06:48 +0900

    修正

--- WebContent/js/project_progress_list.js
+++ WebContent/js/project_progress_list.js
@@ -1,9 +1,18 @@
-/**
- *
- */
+
+
 
 $(function() {
 	$(".modal-delete").click(function(){
+		$("#delete-prepare").fadeIn();
+	});
+	$(".modal-close").click(function(){
+		$("#delete-prepare").fadeOut();
+	});
+});
+
+$(function() {
+	$(".delete-true").click(function(){
+		$("#delete-prepare").fadeOut();
 		$("#delete").fadeIn();
 
 		var index = $(".modal-delete").index(this);
@@ -13,25 +22,3 @@ $(function() {
 		$("#delete").fadeOut();
 	});
 });
-
-$(function(){
-	$(".modal-edit").click(function(){
-$("#edit").fadeIn();
-
-var index=$(".modal-edit").index(this);
-$("#edit-projectid").val($(".projectid").eq(index).text());
-$("#edit-projectday").val($(".projectDay").eq(index).text());
-$("#edit-project").val($(".project").eq(index).text());
-$("#edit-projectplan").val($(".projectPlan").eq(index).text());
-$("#edit-projectresult").val($(".projectResult").eq(index).text());
-$("#edit-other").val($(".other").eq(index).text());
-
-	});
-	$(".modal-close").click(function(){
-		$("#edit").fadeOut();
-	});
-
-});
-
-
-
