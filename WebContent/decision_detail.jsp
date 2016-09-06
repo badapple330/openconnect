abd98fe52f80c69b0eca01080566f7071f361299
tatuhumi ito <forest-of-earth@docomo.ne.jp>
2016-09-06 13:42:08 +0900

    決裁電子化追加

--- WebContent/decision_detail.jsp
+++ WebContent/decision_detail.jsp
@@ -66,6 +66,30 @@
 	<br>
 	<br>
 
+	<div id="modal-main">
+		<!-- #contents START -->
+		ID <input type="text" id="delete-projectid" readonly> <br>
+		プロジェクト名<input type="text" id="delete-projectday" readonly> <br>
+		プロジェクト名<input type="text" id="delete-projectname" readonly> <br>決裁分類
+		<input type="text" id="delete-projectplan" readonly> <br>進捗結果<input
+			type="text" id="delete-projectresult" readonly> <br>申請・承認状況<input
+			type="text" name="projectId" id="delete-other" readonly> <br>
+		<input type="button" class="delete-true button" value="削除"> <input
+			type="button" class="modal-close button" value="閉じる">
+
+		<div class="delete-prepare">
+			本当に削除しますか？
+			<s:form action="DecisionDetailDelete">
+				<input type="hidden" name="decisionId" value="" id="true-delete">
+				<input type="submit" class="delete-true button" value="はい">
+				<input type="button" class="modal-close button" value="いいえ">
+			</s:form>
+
+
+		</div>
+	</div>
+
+
 	<!-- 	戻る -->
 	<s:form action="GetAddressAction">
 		<s:submit value="戻る" />
---
+++ src/com/internousdev/openconnect/decisionDetail/dao/DecisionDetailDeleteDAO.java
@@ -0,0 +1,51 @@
+/**
+ *
+ */
+package com.internousdev.openconnect.decisionDetail.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.internousdev.util.DBConnector;
+
+/**
+ * 画面で受け取ったプロジェクトIDを元にして、DBからそのIDの情報を削除する為のクラス
+ * @author TATUHUMI ITOU
+ * @since 2016/09/04
+ * @version 1.0
+ */
+public class DecisionDetailDeleteDAO {
+	 /**
+     * 削除メソッド  画面で受け取ったプロジェクトIDを元にして、DBからそのIDの情報を削除する為のメソッド
+     * @author TATUHUMI ITOU
+     */
+	public int delete(int decisionId){
+
+		int count =0;
+
+		DBConnector db = new DBConnector("com.mysql.jdbc.Driver","jdbc:mysql://localhost/","openconnect","root","mysql");
+		Connection conn = db.getConnection();
+
+		String sql = "delete from decision_detail where decision_id = ?";
+
+		System.out.println(decisionId);
+		try{
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, decisionId);
+			count = ps.executeUpdate();
+			System.out.println(count);
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally {
+			try{
+				conn.close();
+			}catch(SQLException e){
+				e.printStackTrace();
+			}
+		}
+		System.out.println("last:" + count);
+		return count;
+	}
+
+}
