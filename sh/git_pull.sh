699ff96be8f236483d623ddb4dbf9355dcd3f485
Takuma Inoue <takumainoue7@gmail.com>
2016-09-06 17:16:39 +0900

    shファイル、batファイル作成（主にinternousdevdoc）

---
+++ sh/git_pull.sh
@@ -0,0 +1,67 @@
+cd /opt/tomcat8/webapps/
+# 201407
+cd bookmanager07
+sudo git pull
+cd ../Shop
+sudo git pull
+# 201408
+# 201409
+cd ../bookmanager09
+sudo git pull
+# 201410
+cd ../bookmanager
+sudo git pull
+cd ../ec_10
+sudo git pull
+# 201411
+cd ../EC_11
+sudo git pull
+# 201412
+cd ../attendance_12
+sudo git pull
+cd ../chikezon
+sudo git pull
+# 201501
+cd ../Oden
+sudo git pull
+cd ../sakatike
+sudo git pull
+cd ../UESTO
+sudo git pull
+# 201502
+cd ../tourtike
+sudo git pull
+cd ../solare
+sudo git pull
+# 201503
+cd ../WorldTravel
+sudo git pull
+cd ../YouJustJewelry
+sudo git pull
+cd ../gpscoffee
+sudo git pull
+# 201504
+cd ../LaPoupee
+sudo git pull
+cd ../UCycle
+sudo git pull
+# 201505
+cd ../LiveTickets
+sudo git pull
+cd ../baseballTicket
+sudo git pull
+cd ../IchigoIchie
+sudo git pull
+# 201506
+cd ../G-Tickets
+sudo git pull
+cd ../MovieTickets
+sudo git pull
+cd ../tBox
+sudo git pull
+# 201507
+cd ../biggalaxy
+sudo git pull
+cd ../k-home
+sudo git pull
+
