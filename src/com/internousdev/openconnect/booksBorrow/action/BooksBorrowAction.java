742f76feffbeefe9761cfc11826d5bebfa41aa29
tatsuya.hoshi <internous@00196284658414>
2016-09-07 14:02:14 +0900

    更新

--- src/com/internousdev/openconnect/booksBorrow/action/BooksBorrowAction.java
+++ src/com/internousdev/openconnect/booksBorrow/action/BooksBorrowAction.java
@@ -8,15 +8,22 @@ import com.internousdev.openconnect.booksBorrow.dto.BooksBorrowDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
- * @author internous
- *
+ * DBの情報を画面に表示する為のクラス
+ * @author Tatsuya Hoshi
  */
 public class BooksBorrowAction extends ActionSupport {
 
 	private List<BooksBorrowDTO>bookList=new ArrayList<BooksBorrowDTO>();
 
+	/**
+	 * シリアルバージョンID
+	 */
 	private static final long serialVersionUID = -7586577377473680450L;
 
+	/**
+	 * DAOに入力されたデータを渡して、結果を返す
+	 * @return result データベースに格納できたらSUCCESS、失敗したらERROR
+	 */
 	public String execute() {
 		String result = ERROR;
 		BooksBorrowDAO dao = new BooksBorrowDAO();
--- src/com/internousdev/openconnect/booksBorrow/dto/BooksBorrowDTO.java
+++ src/com/internousdev/openconnect/booksBorrow/dto/BooksBorrowDTO.java
@@ -10,31 +10,70 @@ package com.internousdev.openconnect.booksBorrow.dto;
 public class BooksBorrowDTO {
 
 	private int bookId;
-	private String title;
+	private String borrowStatus;
 	private String borrowHuman;
 	private String borrowDay;
-
+	/**
+	* 取得メソッド
+	* @author
+	* @return
+	*/
 	public int getBookId() {
 		return bookId;
 	}
+	/**
+	* 設定メソッド
+	* @author
+	* @param
+	*/
 	public void setBookId(int bookId) {
 		this.bookId = bookId;
 	}
-	public String getTitle() {
-		return title;
+	/**
+	* 取得メソッド
+	* @author
+	* @return
+	*/
+	public String getBorrowStatus() {
+		return borrowStatus;
 	}
-	public void setTitle(String title) {
-		this.title = title;
+	/**
+	* 設定メソッド
+	* @author
+	* @param
+	*/
+	public void setBorrowStatus(String borrowStatus) {
+		this.borrowStatus = borrowStatus;
 	}
+	/**
+	* 取得メソッド
+	* @author
+	* @return
+	*/
 	public String getBorrowHuman() {
 		return borrowHuman;
 	}
+	/**
+	* 設定メソッド
+	* @author
+	* @param
+	*/
 	public void setBorrowHuman(String borrowHuman) {
 		this.borrowHuman = borrowHuman;
 	}
+	/**
+	* 取得メソッド
+	* @author
+	* @return
+	*/
 	public String getBorrowDay() {
 		return borrowDay;
 	}
+	/**
+	* 設定メソッド
+	* @author
+	* @param
+	*/
 	public void setBorrowDay(String borrowDay) {
 		this.borrowDay = borrowDay;
 	}
