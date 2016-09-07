51b78f3e877b9aa8b0671fa3d1cd1ed5cbf15852
yuichi <yuichi@gmail,com>
2016-09-07 13:41:47 +0900

    更新

--- src/com/internousdev/openconnect/projects/action/ProjectsAction.java
+++ src/com/internousdev/openconnect/projects/action/ProjectsAction.java
@@ -4,14 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.internousdev.openconnect.projects.dao.ProjectsListDAO;
-import com.internousdev.openconnect.projects.dto.ProjectListDTO;
+import com.internousdev.openconnect.projects.dto.ProjectsListDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
+/**
+DB情報を画面に表示させるクラス
+* @author YUICHI KIRIU
+ * @since 2016/09/7
+ * @version 1.0
+ */
 public class ProjectsAction extends ActionSupport{
 
-	private List<ProjectListDTO> projectList = new ArrayList<ProjectListDTO>();
+	private List<ProjectsListDTO> projectList = new ArrayList<ProjectsListDTO>();
 
-
+	/**
+	 * 実行メソッド DAOに入力されたデータを渡して、結果を返す
+	 * @author YUICHI KIRIU
+	 * @return result データベースに格納できたらSUCCESS、失敗したらERROR
+	 */
 				public String execute() {
 					String result = ERROR;
 					ProjectsListDAO dao = new ProjectsListDAO();
@@ -22,13 +32,21 @@ public class ProjectsAction extends ActionSupport{
 					return result;
 				}
 
-				public List<ProjectListDTO> getProjectList() {
+				/**
+				* 取得メソッド
+				* @author  YUICHI KIRIU
+				* @return projectList
+				*/
+				public List<ProjectsListDTO> getProjectList() {
 					return projectList;
 				}
-
-				public void setProjectList(List<ProjectListDTO> projectList) {
+				/**
+				* 設定メソッド
+				* @author  YUICHI KIRIU
+				* @param projectList
+				*/
+				public void setProjectList(List<ProjectsListDTO> projectList) {
 					this.projectList = projectList;
 				}
-
 
 			}
--- src/com/internousdev/openconnect/projects/dao/ProjectsSerachDAO.java
+++ src/com/internousdev/openconnect/projects/dao/ProjectsSerachDAO.java
@@ -7,35 +7,44 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.internousdev.openconnect.projects.dto.ProjectSearchDTO;
+import com.internousdev.openconnect.projects.dto.ProjectsSearchDTO;
 import com.internousdev.util.DBConnector;
 
 public class ProjectsSerachDAO {
 
-	public List<ProjectSearchDTO> searchList = new ArrayList<ProjectSearchDTO>();
+	 /**
+     * 検索したＤＢ情報をリスト化して抽出し、ＤＴＯに格納する
+     *
+     * @author YUICHI KIRIU
+     * @return  searchList
+     */
+
+	public List<ProjectsSearchDTO> searchList = new ArrayList<ProjectsSearchDTO>();
 
 
 
-    public List<ProjectSearchDTO> select(String search) {
+    public List<ProjectsSearchDTO> select(String search) {
         DBConnector db = new DBConnector("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/", "openconnect", "root",
                 "mysql");
         Connection con = db.getConnection();
 
         try {
-            String sql = "SELECT*FROM project WHERE project_name LIKE '%" + search + "%'";
+            String sql = "SELECT*FROM projects WHERE project_name LIKE '%" + search + "%'";
             PreparedStatement ps = con.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
 
             while (rs.next()) {
-            	ProjectSearchDTO dto = new ProjectSearchDTO();
+            	ProjectsSearchDTO dto = new ProjectsSearchDTO();
 
-            	dto.setUserId(rs.getString("user_id"));
+            	dto.setProjectId(rs.getString("project_id"));
             	dto.setProjectName(rs.getString("project_name"));
-            	dto.setManager(rs.getString("manager"));
-            	dto.setSubManager(rs.getString("sub_manager"));
+            	dto.setManagerName(rs.getString("manager_name"));
+            	dto.setSubManagerName(rs.getString("sub_manager_name"));
             	dto.setStartDate(rs.getString("start_date"));
             	dto.setEndDate(rs.getString("end_date"));
-             	dto.setNote(rs.getString("note"));
+            	dto.setNote(rs.getString("note"));
+            	dto.setManagerId(rs.getString("manager_id"));
+            	dto.setSubManagerId(rs.getString("sub_manager_id"));
                 searchList.add(dto);
             }
         } catch (SQLException e) {
@@ -50,11 +59,7 @@ public class ProjectsSerachDAO {
         return searchList;
     }
 
-    /**
+}
+  /**
      * @return selectList
      */
-
-
-
-
-}
