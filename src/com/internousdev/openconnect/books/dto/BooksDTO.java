0bcd9d716730eabcdf0ed4f924e5e690c0612f69
tatsuya.hoshi <internous@00196284658414>
2016-09-07 13:46:52 +0900

    更新

--- src/com/internousdev/openconnect/books/dto/BooksDTO.java
+++ src/com/internousdev/openconnect/books/dto/BooksDTO.java
@@ -11,20 +11,39 @@ public class BooksDTO {
 
 	private int bookId;
 	private String title;
-
-
+	/**
+	* 取得メソッド
+	* @author Tatsuya Hoshi
+	* @return
+	*/
 	public int getBookId() {
 		return bookId;
 	}
+	/**
+	* 設定メソッド
+	* @author Tatsuya Hoshi
+	* @param
+	*/
 	public void setBookId(int bookId) {
 		this.bookId = bookId;
 	}
+	/**
+	* 取得メソッド
+	* @author Tatsuya Hoshi
+	* @return
+	*/
 	public String getTitle() {
 		return title;
 	}
+	/**
+	* 設定メソッド
+	* @author Tatsuya Hoshi
+	* @param
+	*/
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 
 
 
