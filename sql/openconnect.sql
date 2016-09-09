2df1710a4eea6890799e4c977761c2bbfc9f0427
Kohei Nitabaru <nitabaru@gmail.com>
2016-09-09 16:54:56 +0900

    sqlの勤怠更新の部分再変更

--- sql/openconnect.sql
+++ sql/openconnect.sql
@@ -147,7 +147,7 @@ insert project_status(project_id, a_envstart)values
 create table attendance(
 date DATE not null comment '日付',
 user_id int not null comment '生徒ID',
-attendance enum('0','1','2','3','4','5') default '1'  comment '出欠状況',
+attendance enum('0','1','2','3','4','5') default '0'  comment '出欠状況',
 interview enum('1','2') default '2' comment '面談状況',
 foreign key(user_id) references users(user_id)
 );
