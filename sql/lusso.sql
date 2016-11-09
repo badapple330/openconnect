e058646394f22ca633a5ece8f6b1eb4706e9ea50
HIDEYA <HIDEYA@macbookpportdoc>
2016-11-09 13:53:15 +0900

    変更

--- sql/lusso.sql
+++ sql/lusso.sql
@@ -28,4 +28,19 @@ foreign key(master_id) references alcohol_category(master_id)
 insert into menu() values
 (1,1,"GUINNESS","guinness.jpg","delicious","karaage","karaage.jpg","delicious");
 
+create table user(
+user_id int  primary key not null,
+alcohol_name int not null,
+food_name varchar(10) not null ,
+delivery_date date not null,
+total_fee int default 2500,
+name_e VARCHAR(50) NOT NULL,
+credit_number VARCHAR(16) NOT NULL UNIQUE,
+security_code int NOT NULL,
+update_day datetime NOT NULL,
+expiration_day VARCHAR(7) NOT NULL,
+foreign key(user_id)references openconnect.users(user_id)
+);
+
+
 
