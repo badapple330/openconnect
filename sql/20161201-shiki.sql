3f4f489302925d963ee3cbc86786f25a760b8832
Ranna Miyake <ramaway.m@gmail.com>
2016-12-07 14:22:56 +0900

    shiki sql

--- sql/20161201-shiki.sql
+++ sql/20161201-shiki.sql
@@ -5,43 +5,43 @@ use shiki;
 
 create table card(
 	card_num int not null primary key,
-	card_name varchar(255) not null
+	card varchar(255) not null
 ) engine=InnoDB;
 
-insert into card(card_num, card_name) values(0, "visa");
-insert into card(card_num, card_name) values(1, "master");
-insert into card(card_num, card_name) values(2, "american express");
+insert into card(card_num, card) values(0, "visa");
+insert into card(card_num, card) values(1, "master");
+insert into card(card_num, card) values(2, "american express");
 
 create table season(
   season_num int not null primary key,
-  season_name varchar(255) not null
+  season varchar(255) not null
 ) engine=InnoDB;
 
-insert into season(season_num, season_name) values(0, "Spring");
-insert into season(season_num, season_name) values(1, "Summer");
-insert into season(season_num, season_name) values(2, "Autumn");
-insert into season(season_num, season_name) values(3, "Winter");
+insert into season(season_num, season) values(0, "Spring");
+insert into season(season_num, season) values(1, "Summer");
+insert into season(season_num, season) values(2, "Autumn");
+insert into season(season_num, season) values(3, "Winter");
 
 create table gender(
   gender_num int not null primary key,
-  gender_name varchar(255) not null
+  gender varchar(255) not null
 ) engine=InnoDB;
 
-insert into gender(gender_num, gender_name) values(0, "not known");
-insert into gender(gender_num, gender_name) values(1, "male");
-insert into gender(gender_num, gender_name) values(2, "female");
-insert into gender(gender_num, gender_name) values(9, "not applicable");
+insert into gender(gender_num, gender) values(0, "not known");
+insert into gender(gender_num, gender) values(1, "male");
+insert into gender(gender_num, gender) values(2, "female");
+insert into gender(gender_num, gender) values(9, "not applicable");
 
 create table satisfaction(
-  satisfaction_id int not null primary key,
-  satisfaction_name varchar(255) not null
+  satisfaction_num int not null primary key,
+  satisfaction varchar(255) not null
 ) engine=InnoDB;
 
-insert into satisfaction(satisfaction_id, satisfaction_name) values(0, "Very dissatisfied");
-insert into satisfaction(satisfaction_id, satisfaction_name) values(1, "dissatisfied");
-insert into satisfaction(satisfaction_id, satisfaction_name) values(2, "Neutral");
-insert into satisfaction(satisfaction_id, satisfaction_name) values(3, "Satisfied");
-insert into satisfaction(satisfaction_id, satisfaction_name) values(4, "Very satisfied");
+insert into satisfaction(satisfaction_num, satisfaction) values(0, "Very dissatisfied");
+insert into satisfaction(satisfaction_num, satisfaction) values(1, "dissatisfied");
+insert into satisfaction(satisfaction_num, satisfaction) values(2, "Neutral");
+insert into satisfaction(satisfaction_num, satisfaction) values(3, "Satisfied");
+insert into satisfaction(satisfaction_num, satisfaction) values(4, "Very satisfied");
 
 create table questionnaire(
   age int not null,
@@ -53,31 +53,35 @@ create table questionnaire(
 
 create table payment(
 	payment_num int not null primary key,
-	payment_name varchar(255)
+	payment varchar(255)
 );
 
-insert into payment(payment_num, payment_name) values(0, "Bank Transfer");
-insert into payment(payment_num, payment_name) values(1, "Credit Card");
+insert into payment(payment_num, payment) values(0, "Bank account instant transfer");
+insert into payment(payment_num, payment) values(1, "Credit card");
 
 create table tour(
-  user_id int not null,
   tour_id int not null,
-  mail_address varchar(255) not null,
   tour_name varchar(100) not null,
   tour_date date not null,
   tour_godate date not null,
-  tour_returndate date not null,
   tour_category int not null,
   tour_amount int not null,
   tour_detail text not null,
- img_address001 text not null comment '画像001',
-img_address002 text not null comment '画像002',
-img_address003 text not null comment '画像003',
-img_address004 text not null comment '画像004',
-img_address005 text not null comment '画像005',
-  foreign key(user_id) references openconnect.users(user_id) on delete cascade,
+  img varchar(255) not null,
   foreign key(tour_category) references season(season_num) on delete cascade
 );
+
+create table users(
+  user_id int not null,
+  mail_address varchar(255) not null,
+  password varchar(255) not null,
+  login_flg boolean not null,
+  user_flg int not null default 3
+);
+
+insert into users(user_id, mail_address, password, login_flg, user_flg) values(0, "aa@gmail.com", "password", FALSE, 3);
+insert into users(user_id, mail_address, password, login_flg, user_flg) values(1, "bb@gmail.com", "password", FALSE, 3);
+insert into users(user_id, mail_address, password, login_flg, user_flg) values(2, "cc@gmail.com", "password", FALSE, 3);
 
 create table credit(
   credit_type int not null,
@@ -87,7 +91,6 @@ create table credit(
   expiration_month varchar(2) not null,
   expiration_year int(4) not null,
   payment_method int not null,
-  spend int not null,
   foreign key(credit_type) references card(card_num) on delete cascade,
   foreign key(payment_method) references payment(payment_num) on delete cascade
 );
