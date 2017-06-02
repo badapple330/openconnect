e2d1c8e38ff581411d52417bc5a3f4ddf38f488f
Teppei Matsumoto <tetukabe_work@yahoo.co.jp>
2017-06-02 17:53:47 +0900

    出欠修正

--- src/com/internousdev/openconnect/attendance/action/AttendanceAction.java
+++ src/com/internousdev/openconnect/attendance/action/AttendanceAction.java
@@ -3,6 +3,10 @@
  */
 package com.internousdev.openconnect.attendance.action;
 
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
 import com.internousdev.openconnect.attendance.dao.AttendanceDAO;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -13,7 +17,7 @@ import com.opensymphony.xwork2.ActionSupport;
  *@since 2017/06/02
  *@version 1.0
  */
-public class AttendanceAction extends ActionSupport{
+public class AttendanceAction extends ActionSupport implements SessionAware {
 
 	/**
 	 * シリアルID
@@ -37,6 +41,11 @@ public class AttendanceAction extends ActionSupport{
 	private String reason;
 
 	/**
+	 * ユーザー情報
+	 */
+	private Map<String, Object> session;
+
+	/**
 	 * 実行メソッド　出欠状況の送信処理をする
 	 * @author Teppei Matsumoto
 	 * @since 2017/06/02
@@ -46,11 +55,16 @@ public class AttendanceAction extends ActionSupport{
 		String result =ERROR;
 
 		AttendanceDAO dao=new AttendanceDAO();
-		  if(!dao.select(user_id)){
-		    int con = dao.insert(user_id,attendance,reason);
+		  if(session.get("user_id")!=null){
+			  int userId=(int)session.get("user_id");
+
+		    int con = dao.insert(userId,attendance,reason);
 		    if(con > 0){
 		      result = SUCCESS;
 				}
+		  }else{
+
+			  result = LOGIN;
 		  }
 		  return result;
 	}
@@ -110,6 +124,20 @@ public class AttendanceAction extends ActionSupport{
 		this.reason = reason;
 	}
 
+	/**
+	 * @return session
+	 */
+	public Map<String, Object> getSession() {
+		return session;
+	}
+
+
+	/**
+	 * @param session セットする session
+	 */
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
 
 
 }
--- src/com/internousdev/openconnect/attendance/dao/AttendanceDAO.java
+++ src/com/internousdev/openconnect/attendance/dao/AttendanceDAO.java
@@ -3,11 +3,12 @@
  */
 package com.internousdev.openconnect.attendance.dao;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.TimeZone;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import com.internousdev.util.DBConnector;
+
 
 /**
  * 出席状況についてデータベースに問い合わせをおこないます。
@@ -21,24 +22,34 @@ public class AttendanceDAO {
 	 *@param user_id 	ユーザー番号
 	 *@param attendance 出欠状況
 	 *@param reason		備考
+	 *@param atDate		日時
 	 *@return ret
 	 */
 
-	public int insert(int user_id,int attendance,String reason){
+	public int insert(int userId,int attendance,String reason){
 
 		DBConnector db=new DBConnector("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/", "openconnect", "root","mysql");
 		Connection con =db.getConnection();
-		int count = 0;
+		int ret = 0;
+		String sql = "insert into attendance(user_id,attendance,reason) values(?,?,?)";
 
-		/* 管理者が日本人であることを想定しているため、
-		   日本のタイムゾーンで現在の日付を取得 */
-		Calendar cal = Calendar.getInstance();
-		TimeZone tz = TimeZone.getTimeZone("Asia/Tokyo");
-		cal.setTimeZone(tz);
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
-		String dt = sdf.format(cal.getTime());
-
-
+		try{
+	            PreparedStatement ps = con.prepareStatement(sql);
+	            ps.setInt(1, userId);
+	            ps.setInt(2, attendance);
+	            ps.setString(3, reason);
+	            ret = ps.executeUpdate();
+	        }catch (SQLException e){
+	            e.printStackTrace();
+	        }finally{
+	            if(con != null){
+	                try{
+	                    con.close();
+	                }catch (SQLException e){
+	                    e.printStackTrace();
+	                }
+	            }
+	        }
+		return ret;
 	}
-
 }
