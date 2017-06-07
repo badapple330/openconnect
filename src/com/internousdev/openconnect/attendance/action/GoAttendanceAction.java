e106be4dcd66c1680c3fa2ae03a0cd2a6bbeb0c9
Teppei Matsumoto <tetukabe_work@yahoo.co.jp>
2017-06-07 13:18:20 +0900

    勤怠更新　Java/strutsにGoAttを復元

---
+++ src/com/internousdev/openconnect/attendance/action/GoAttendanceAction.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package com.internousdev.openconnect.attendance.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+** GoAttendanceAction 管理者画面にログインするためのアクション
+ * @author Teppei Matsumoto
+ * @since  2017/06/07
+ * @version 1.0
+ */
+public class GoAttendanceAction extends ActionSupport{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1181846755041839614L;
+
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+
+}
