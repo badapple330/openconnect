981fe9c34d5dbe43d5289162e11466024b67aebc
Sayaka Fukuda <skxx505@gmail.com>
2017-06-09 16:42:45 +0900

    TOPにスライドショー画像とかつけてみました

--- WebContent/js/applist.js
+++ WebContent/js/applist.js
@@ -1,20 +1,30 @@
-function slideSwitch() {
-   var $active = $('#slideshow img.active');
 
-   if ( $active.length == 0 ) $active = $('#slideshow img:last');
+/*画像スライドショー----------------------------------------------------*/
 
-   var $next =  $active.next().length ? $active.next()
-      : $('#slideshow img:first');
-
-   $active.addClass('last-active');
-
-   $next.css({opacity: 0.0})
-      .addClass('active')
-      .animate({opacity: 1.0}, 1000, function() {
-           $active.removeClass('active last-active');
-      });
+myImage = new Array(	// 画像ファイル名の設定
+"img/sity.jpg",		//街
+"img/sky.jpg",		//気球と空
+"img/drink.jpg",		//ビール
+"img/social.jpg",		//すまほ
+"img/sunset.jpg",		//夕日
+"img/candy.jpg"		//宇宙
+);
+myNowCnt = -1;		// 現在表示している配列番号
+myflg = 0;		// どっちを表示して、どっちを消すかのフラグ
+function myChange(){	// スライドショーメイン関数
+	myNowCnt = (myNowCnt<myImage.length-1) ? myNowCnt+1 : 0;		// 次の配列番号
+	myflg = (myflg==0) ? 1 : 0;						// 表示・非表示フラグ反転
+	if (myflg == 0){
+		document.getElementById("idshow1").src = myImage[myNowCnt];		// 次の画像をセットする
+		document.getElementById("idshow1").className = "fadein";		// フェードイン
+		document.getElementById("idshow2").className = "fadeout";	// フェードアウト
+	}else{
+		document.getElementById("idshow2").src = myImage[myNowCnt];		// 次の画像をセットする
+		document.getElementById("idshow1").className = "fadeout";	// フェードアウト
+		document.getElementById("idshow2").className = "fadein";		// フェードイン
+	}
+	setTimeout( "myChange()" , 5000 );					// 4秒周期に画像を更新する
 }
 
-$(function() {
-   setInterval( "slideSwitch()", 3000 );
-});
+/*画像スライドショーここまで---------------------------------------------*/
+
