0dc1f2f8e930c7cf42d101af4563f0d17ca7d6af
KonosukeKawamura <gito_rgoi.kmii-kroara@ezweb.ne.jp>
2017-06-19 14:51:50 +0900

    attendanceDaoの更新

--- src/com/internousdev/openconnect/attendance_test/AttendanceDao.java
+++ src/com/internousdev/openconnect/attendance_test/AttendanceDao.java
@@ -3,15 +3,66 @@
  */
 package com.internousdev.openconnect.attendance_test;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import com.internousdev.openconnect.attendance.dto.AttendanceDTO;
+import com.internousdev.util.DBConnector;
 
 /**
- * @author internousdev
- *
+ * 勤怠の検索に関するDAO
+ * @author Kawamura
+ * @since6/19
+ * @version1.0.0
  */
 public class AttendanceDao {
+
+	/**
+	 * 勤怠履歴を格納する
+	 */
+	public ArrayList<AttendanceDTO> searchList  = new ArrayList<AttendanceDTO>();
+
+	/**
+	 * 勤怠状況で名前を取得するメソッド
+	 * @param attendance 勤怠
+	 * @return adminhistorylist 勤怠登録履歴
+	 */
+	public ArrayList<AttendanceDTO> select1(String attendance) {
+		DBConnector db = new DBConnector("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/", "openconnect", "root", "mysql");
+		Connection con = db.getConnection();
+
+		ArrayList<AttendanceDTO> searchList = new ArrayList<AttendanceDTO>();
+
+		String sql = null;
+
+		try {
+			PreparedStatement ps = con.prepareStatement(sql); //「?」のパラメーターを持つSQLを実行するためのインターフェイス。SQLコンテナ
+
+			ResultSet rs = ps.executeQuery(); //SQL文の実行インターフェース。
+
+			while (rs.next()) {
+				AttendanceDTO dto = new AttendanceDTO();
+				dto.setFamilyNameKanji(rs.getString("family_name_kanji"));
+				dto.setGivenNameKanji(rs.getString("given_name_kanji"));
+				searchList.add(dto);
+			}
+			rs.close();
+			ps.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return searchList;
+	}
 
 	/**
 	 * @param attendance
