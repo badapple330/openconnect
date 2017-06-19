f5a981a43a00e371281c44beb2e9b9b79751ec34
Nozawa Hideyuki <gt1tawashi@gmail.com>
2017-06-19 15:32:43 +0900

    strutsにDecisionPreviewActionを追加しました。

--- src/com/internousdev/openconnect/decision/action/DecisionPreviewAction.java
+++ src/com/internousdev/openconnect/decision/action/DecisionPreviewAction.java
@@ -1,20 +1,626 @@
 /**
- * 
+ *
  */
 package com.internousdev.openconnect.decision.action;
 
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.omg.CORBA.portable.UnknownException;
+
+import com.internousdev.openconnect.decision.dao.DecisionPreviewDAO;
+import com.internousdev.openconnect.decision.dto.DecisionDTO;
+import com.opensymphony.xwork2.ActionSupport;
+
 /**
- * @author internousdev
+ * @author Hideyuki Nozawa
+ * @since 2017/06/19
+ * @version 1.0
  *
  */
-public class DecisionPreviewAction {
+public class DecisionPreviewAction extends ActionSupport{
 
 	/**
-	 * @param args
+	 * 決裁ID
+	 *
 	 */
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	private int decisionId;
 
+	/**
+	 * ユーザーID
+	 *
+	 */
+	private int userId;
+
+	/**
+	 * 案件名
+	 *
+	 */
+	private String decisionName;
+
+	/**
+	 * 実施起案番号
+	 *
+	 */
+	private String iDraftingId;
+	/**
+	 * 実施決裁番号
+	 *
+	 */
+	private String iApprovalId;
+	/**
+	 * 契約起案番号
+	 *
+	 */
+	private String aDraftingId;
+	/**
+	 * 契約決裁番号
+	 *
+	 */
+	private String cdId;
+	/**
+	 * 実施兼契約起案番号
+	 *
+	 */
+	private String iADId;
+	/**
+	 * 実施兼契約番号
+	 *
+	 */
+
+	private String iAId;
+
+	/**
+	 * 承認番号
+	 */
+	private String adminNum;
+
+	/**
+	 * 理由・目的
+	 */
+	public String cause;
+
+
+	/**
+	 * 	頭紙文章
+	 */
+	private String head;
+
+	/**
+	 * 建設費用
+	 */
+	private float bildCost;
+
+	/**
+	 * 損益費用
+	 */
+	private float benefit;
+
+	/**
+	 * 合計金額
+	 */
+	private float amountAll;
+
+	/**
+	 * 概要
+	 */
+	public String summary;
+
+	/**
+	 * 開始日
+	 */
+	public String startDay;
+
+	/**
+	 * 終了日
+	 */
+	public String endDay;
+
+
+	/**
+	 * 開発端末料
+	 */
+	public int prove;
+
+	/**
+	 * リリース環境使用料
+	 */
+	public float re;
+
+
+	/**
+	 *回線使用料
+	 */
+	public int line;
+
+	/**
+	 *施設使用料
+	 */
+	public float room;
+
+	/**
+	 *開発要員
+	 */
+	public int human;
+
+	/**
+	 *雑費
+	 */
+	public float etc;
+
+	/**
+	 * 決裁手続きの情報をリスト化
+	 */
+	private ArrayList<DecisionDTO> decisionPreviewList = new  ArrayList<DecisionDTO>();
+
+	/**
+	 * 起案者の情報のリスト
+	 */
+	private ArrayList<DecisionDTO> nameList = new ArrayList<DecisionDTO>();
+
+	/**
+	 * セッション情報
+	 */
+	private Map<String,Object> session;
+
+
+	public String execute(){
+		String result = ERROR;
+		DecisionPreviewDAO dao = new DecisionPreviewDAO();
+		System.out.println(decisionId);
+		try {
+		decisionPreviewList=dao.select(decisionId);
+		} catch (UnknownException e) {
+		e.printStackTrace();
+		}
+
+		if(decisionPreviewList!=null){
+			System.out.println(userId);
+			try {
+				nameList = dao.selectByUserId(userId);
+			} catch (UnknownException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		result=SUCCESS;
+		return result;
+		}
+
+
+	/**
+	 * @return userId
+	 */
+	public int getDecisionId() {
+		return decisionId;
 	}
+
+	/**
+	 * @param userId セットする userId
+	 */
+	public void setDecisionId(int decisionId) {
+		this.decisionId = decisionId;
+	}
+	/**
+	 * @return userId
+	 */
+	public int getUserId() {
+		return userId;
+	}
+
+	/**
+	 * @param userId セットする userId
+	 */
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return decisionName
+	 */
+	public String getDecisionName() {
+		return decisionName;
+	}
+
+	/**
+	 * @param decisionName セットする decisionName
+	 */
+	public void setDecisionName(String decisionName) {
+		this.decisionName = decisionName;
+	}
+
+	/**
+	 * @return iDraftingId
+	 */
+	public String getiDraftingId() {
+		return iDraftingId;
+	}
+
+	/**
+	 * @param iDraftingId セットする iDraftingId
+	 */
+	public void setiDraftingId(String iDraftingId) {
+		this.iDraftingId = iDraftingId;
+	}
+
+	/**
+	 * @return iApprovalId
+	 */
+	public String getiApprovalId() {
+		return iApprovalId;
+	}
+
+	/**
+	 * @param iApprovalId セットする iApprovalId
+	 */
+	public void setiApprovalId(String iApprovalId) {
+		this.iApprovalId = iApprovalId;
+	}
+
+	/**
+	 * @return aDraftingId
+	 */
+	public String getaDraftingId() {
+		return aDraftingId;
+	}
+
+	/**
+	 * @param aDraftingId セットする aDraftingId
+	 */
+	public void setaDraftingId(String aDraftingId) {
+		this.aDraftingId = aDraftingId;
+	}
+
+	/**
+	 * @return cdId
+	 */
+	public String getCdId() {
+		return cdId;
+	}
+
+	/**
+	 * @param cdId セットする cdId
+	 */
+	public void setCdId(String cdId) {
+		this.cdId = cdId;
+	}
+
+	/**
+	 * @return iADId
+	 */
+	public String getiADId() {
+		return iADId;
+	}
+
+	/**
+	 * @param iADId セットする iADId
+	 */
+	public void setiADId(String iADId) {
+		this.iADId = iADId;
+	}
+
+	/**
+	 * @return iAId
+	 */
+	public String getiAId() {
+		return iAId;
+	}
+
+	/**
+	 * @param iAId セットする iAId
+	 */
+	public void setiAId(String iAId) {
+		this.iAId = iAId;
+	}
+
+	/**
+	 * @return adminNum
+	 */
+	public String getAdminNum() {
+		return adminNum;
+	}
+
+	/**
+	 * @param adminNum セットする adminNum
+	 */
+	public void setAdminNum(String adminNum) {
+		this.adminNum = adminNum;
+	}
+
+	/**
+	 * @return head
+	 */
+	public String getHead() {
+		return head;
+	}
+
+	/**
+	 * @param head セットする head
+	 */
+	public void setHead(String head) {
+		this.head = head;
+	}
+
+	/**
+	 * @return bildCost
+	 */
+	public float getBildCost() {
+		return bildCost;
+	}
+
+	/**
+	 * @param bildCost セットする bildCost
+	 */
+	public void setBildCost(float bildCost) {
+		this.bildCost = bildCost;
+	}
+
+	/**
+	 * @return benefit
+	 */
+	public float getBenefit() {
+		return benefit;
+	}
+
+	/**
+	 * @param benefit セットする benefit
+	 */
+	public void setBenefit(float benefit) {
+		this.benefit = benefit;
+	}
+
+	/**
+	 * @return amountAll
+	 */
+	public float getAmountAll() {
+		return amountAll;
+	}
+
+	/**
+	 * @param amountAll セットする amountAll
+	 */
+	public void setAmountAll(float amountAll) {
+		this.amountAll = amountAll;
+	}
+
+	/**
+	 * @return session
+	 */
+	public Map<String,Object> getSession() {
+		return session;
+	}
+
+	/**
+	 * @param session セットする session
+	 */
+	public void setSession(Map<String,Object> session) {
+		this.session = session;
+	}
+
+	/**
+	 * @return decisionPreviewList
+	 */
+	public ArrayList<DecisionDTO> getdecisionPreviewList() {
+		return decisionPreviewList;
+	}
+
+	/**
+	 * @param decisionPreviewList セットする decisionPreviewList
+	 */
+	public void setdecisionPreviewList(ArrayList<DecisionDTO> decisionPreviewList) {
+		this.decisionPreviewList = decisionPreviewList;
+	}
+
+
+
+
+	/**
+	* 取得メソッド を取得
+	* @author KOHEI NITABARU
+	* @return nameList
+	*/
+	public ArrayList<DecisionDTO> getNameList() {
+		return nameList;
+	}
+
+	/**
+	* 設定メソッド を設定
+	* @author KOHEI NITABARU
+	* @param nameList
+	*/
+	public void setNameList(ArrayList<DecisionDTO> nameList) {
+		this.nameList = nameList;
+	}
+
+	/**
+	 * @return cause
+	 */
+	public String getCause() {
+		return cause;
+	}
+
+
+
+
+	/**
+	 * @param cause セットする cause
+	 */
+	public void setCause(String cause) {
+		this.cause = cause;
+	}
+
+
+
+	/**
+	 * @return summary
+	 */
+	public String getSummary() {
+		return summary;
+	}
+
+
+	/**
+	 * @param summary セットする summary
+	 */
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+
+	/**
+	 * @return startDay
+	 */
+	public String getStartDay() {
+		return startDay;
+	}
+
+	/**
+	 * @param startDay セットする startDay
+	 */
+	public void setStartDay(String startDay) {
+		this.startDay = startDay;
+	}
+
+
+	/**
+	 * @return endDay
+	 */
+	public String getEndDay() {
+		return endDay;
+	}
+
+
+
+	/**
+	 * @param endDay セットする endDay
+	 */
+	public void setEndDay(String endDay) {
+		this.endDay = endDay;
+	}
+
+
+
+
+
+
+
+	/**
+	 * @return prove
+	 */
+	public int getProve() {
+		return prove;
+	}
+
+
+
+
+	/**
+	 * @param prove セットする prove
+	 */
+	public void setProve(int prove) {
+		this.prove = prove;
+	}
+
+
+
+
+	/**
+	 * @return re
+	 */
+	public float getRe() {
+		return re;
+	}
+
+
+
+
+	/**
+	 * @param re セットする re
+	 */
+	public void setRe(float re) {
+		this.re = re;
+	}
+
+
+
+
+	/**
+	 * @return line
+	 */
+	public int getLine() {
+		return line;
+	}
+
+
+
+
+	/**
+	 * @param line セットする line
+	 */
+	public void setLine(int line) {
+		this.line = line;
+	}
+
+
+
+
+	/**
+	 * @return room
+	 */
+	public float getRoom() {
+		return room;
+	}
+
+
+
+
+	/**
+	 * @param room セットする room
+	 */
+	public void setRoom(float room) {
+		this.room = room;
+	}
+
+
+
+
+	/**
+	 * @return human
+	 */
+	public int getHuman() {
+		return human;
+	}
+
+
+
+
+	/**
+	 * @param human セットする human
+	 */
+	public void setHuman(int human) {
+		this.human = human;
+	}
+
+
+
+
+	/**
+	 * @return etc
+	 */
+	public float getEtc() {
+		return etc;
+	}
+
+
+
+
+	/**
+	 * @param etc セットする etc
+	 */
+	public void setEtc(float etc) {
+		this.etc = etc;
+	}
+
 
 }
--- src/com/internousdev/openconnect/decision/dao/DecisionPreviewDAO.java
+++ src/com/internousdev/openconnect/decision/dao/DecisionPreviewDAO.java
@@ -1,20 +1,148 @@
 /**
- * 
+ *
  */
 package com.internousdev.openconnect.decision.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.internousdev.openconnect.decision.dto.DecisionDTO;
+import com.internousdev.util.DBConnector;
+
 /**
- * @author internousdev
+ * @author Hideyuki Nozawa
+ * @since 2017/06/19
+ * @version 1.0
  *
  */
 public class DecisionPreviewDAO {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	public ArrayList<DecisionDTO> decisionPreviewList = new ArrayList<DecisionDTO>();
+
+	public ArrayList<DecisionDTO> nameList = new ArrayList<DecisionDTO>();
+
+	public ArrayList<DecisionDTO> select(int decisionId){
+
+		DBConnector db = new DBConnector("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/", "openconnect", "root",
+				"mysql");
+		Connection con = db.getConnection();
+
+		ArrayList<DecisionDTO> decisionPreviewList = new ArrayList<DecisionDTO>();
+
+		String sql = "select * from decision where decision_id=?";
+
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1, decisionId);
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				DecisionDTO dto = new DecisionDTO();
+
+				dto.setUserId(rs.getInt("user_id")); // ユーザーID
+
+				dto.setDecisionName(rs.getString("decision_name")); // 案件名
+
+				dto.setIDraftingId(rs.getString("i_drafting_id")); // 実施起案番号
+
+				dto.setIApprovalId(rs.getString("i_approval_id")); // 実施決裁番号
+
+				dto.setADraftingId(rs.getString("a_drafting_id")); // 契約起案番号
+
+				dto.setCdId(rs.getString("cd_id")); // 契約決裁番号
+
+				dto.setIADId(rs.getString("i_a_d_id")); // 実施兼契約起案番号
+
+				dto.setIAId(rs.getString("i_a_id")); // 実施兼契約番号
+
+				dto.setSummary(rs.getString("summary")); // 概要
+
+				dto.setCause(rs.getString("cause")); // 理由・目的
+
+				dto.setStartDay(rs.getString("start_day")); // 開始日
+
+				dto.setEndDay(rs.getString("end_day")); // 終了日
+
+				dto.setAmountAll(rs.getFloat("amount_all")); // 合計金額
+
+				dto.setBenefit(rs.getFloat("benefit")); // 損益費用
+
+				dto.setBildCost(rs.getFloat("bild_cost")); // 建設費用
+
+				dto.setProve(rs.getInt("prove")); // 開発端末料
+
+				dto.setRe(rs.getFloat("re")); // リリース環境使用量
+
+				dto.setLine(rs.getInt("line")); // 回線使用料
+
+				dto.setRoom(rs.getFloat("room")); // 施設使用料
+
+				dto.setHuman(rs.getInt("human")); // 開発要員
+
+				dto.setEtc(rs.getFloat("etc")); // 雑費
+
+				dto.setPersons(rs.getInt("persons")); // 人数
+
+				dto.setTotalProve(rs.getInt("total_prove")); // 合計開発端末料
+
+				dto.setTotalRe(rs.getFloat("total_re")); // 合計リリース環境使用料
+
+				dto.setTotalLine(rs.getInt("total_line")); // 合計回線使用料
+
+				dto.setTotalRoom(rs.getFloat("total_room")); // 合計施設使用料
+
+				dto.setTotalHuman(rs.getInt("human")); // 合計開発要員
+
+				dto.setTotalEtc(rs.getFloat("etc")); // 合計雑費
+
+				decisionPreviewList.add(dto);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return decisionPreviewList;
 
 	}
+
+	public ArrayList<DecisionDTO> selectByUserId(int userId) {
+		DecisionDTO dto = new DecisionDTO();
+		DBConnector db = new DBConnector("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/",
+				"openconnect", "root", "mysql");
+		Connection con = db.getConnection();
+		ArrayList<DecisionDTO> nameList = new ArrayList<DecisionDTO>();
+		String sql = "select * users from  where user_id=?";
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1,userId);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				dto.setUserId(rs.getInt("user_id")); // ユーザーID
+				dto.setFamilyNameKanji(rs.getString("family_name_kanji")); // 姓（漢字）
+				dto.setGivenNameKanji(rs.getString("given_name_kanji")); // 名（漢字）
+				nameList.add(dto);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return nameList;
+	}
+
 
 }
