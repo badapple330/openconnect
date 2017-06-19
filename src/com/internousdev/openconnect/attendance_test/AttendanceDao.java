6f36fc13163ab822880ce62b5459725edfdbabf2
KonosukeKawamura <gito_rgoi.kmii-kroara@ezweb.ne.jp>
2017-06-19 14:23:37 +0900

    勤怠新機能テストのDAOとACTIONクラスの更新

--- src/com/internousdev/openconnect/attendance_test/AttendanceDao.java
+++ src/com/internousdev/openconnect/attendance_test/AttendanceDao.java
@@ -1,12 +1,25 @@
 /**
- * 
+ *
  */
 package com.internousdev.openconnect.attendance_test;
+
+import java.util.ArrayList;
+
+import com.internousdev.openconnect.attendance.dto.AttendanceDTO;
 
 /**
  * @author internousdev
  *
  */
 public class AttendanceDao {
+
+	/**
+	 * @param attendance
+	 * @return
+	 */
+	public ArrayList<AttendanceDTO> select(String attendance) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
 
 }
--- src/com/internousdev/openconnect/attendance_test/attendanceACTION.java
+++ src/com/internousdev/openconnect/attendance_test/attendanceACTION.java
@@ -1,12 +1,76 @@
 /**
- * 
+ *
  */
 package com.internousdev.openconnect.attendance_test;
 
-/**
- * @author internousdev
- *
- */
-public class attendanceACTION {
+import java.util.ArrayList;
+import java.util.Map;
 
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.internousdev.openconnect.attendance.dto.AttendanceDTO;
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * 出欠状況を検索するクラス
+ * @author 河村
+ * @since 6/19
+ * @version 1.0.0
+ */
+public class attendanceACTION extends ActionSupport implements SessionAware{
+
+	private String attendance;	//勤怠
+
+	/*ユーザーリスト*/
+	private ArrayList<AttendanceDTO> searchList = new ArrayList<AttendanceDTO>();
+
+	public String execute(){
+
+		String result = ERROR;
+
+		AttendanceDao dao = new AttendanceDao();
+		searchList = dao.select(attendance);
+		if(searchList.size() > 0){
+			result=SUCCESS;
+		}
+		return result;
+	}
+
+
+	/**
+	 * searchListを取得します。
+	 * @return searchList
+	 */
+	public ArrayList<AttendanceDTO> getSearchList() {
+		return searchList;
+	}
+
+
+
+	/**
+	 * searchListを設定します。
+	 * @param searchList searchList
+	 */
+	public void setSearchList(ArrayList<AttendanceDTO> searchList) {
+		this.searchList = searchList;
+	}
+
+	/**
+	 * 勤怠を取得します。
+	 * @return attendance 勤怠
+	 */
+	public String getAttendance() {
+		return attendance;
+
+	}
+
+
+	/* (非 Javadoc)
+	 * @see org.apache.struts2.interceptor.SessionAware#setSession(java.util.Map)
+	 */
+	@Override
+	public void setSession(Map<String, Object> arg0) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
 }
