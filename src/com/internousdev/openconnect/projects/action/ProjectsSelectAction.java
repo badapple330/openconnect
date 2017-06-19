d1530ab4ec6a471db591d904afe650658fac7019
Takeshi Ide <streetpursuer@gmail.com>
2017-06-19 10:20:01 +0900

    ProjectSelectAction.javaの赤字部分を修正しました。

--- src/com/internousdev/openconnect/projects/action/ProjectsSelectAction.java
+++ src/com/internousdev/openconnect/projects/action/ProjectsSelectAction.java
@@ -9,14 +9,14 @@ import com.internousdev.openconnect.students.dao.StudentsSelectDAO;
 import com.internousdev.openconnect.students.dto.StudentsDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
-
 /**
  * 検索したＤＢ情報を画面に表示する為のクラス
+ *
  * @author YUICHI KIRIU
  * @since 2016/09/7
  *
  */
-public class ProjectsSelectAction extends ActionSupport{
+public class ProjectsSelectAction extends ActionSupport {
 	/**
 	 * シリアルＩＤ
 	 */
@@ -35,7 +35,7 @@ public class ProjectsSelectAction extends ActionSupport{
 	/**
 	 * 検索リスト
 	 */
-	private List<ProjectsSelectDTO > searchList = new ArrayList<ProjectsSelectDTO>();
+	private List<ProjectsSelectDTO> searchList = new ArrayList<ProjectsSelectDTO>();
 
 	/**
 	 * 受講生リスト
@@ -53,39 +53,45 @@ public class ProjectsSelectAction extends ActionSupport{
 		ProjectsSelectDAO dao = new ProjectsSelectDAO();
 		searchList = dao.select(search);
 
-		for(int i=0;i<searchList.size();i++){
+		for (int i = 0; i < searchList.size(); i++) {
 
 			ProjectsSelectDTO dto = new ProjectsSelectDTO();
 
 			dto = dao.selectId(searchList.get(i).getSubManagerId());
-			searchList.get(i).setSubManagerGivenNameKanji( dto.getSubManagerGivenNameKanji() );
-			searchList.get(i).setSubManagerFamilyNameKanji( dto.getSubManagerFamilyNameKanji() );
+			searchList.get(i).setSubManagerGivenNameKanji(dto.getSubManagerGivenNameKanji());
+			searchList.get(i).setSubManagerFamilyNameKanji(dto.getSubManagerFamilyNameKanji());
 		}
 
 		if (searchList.size() != 0) {
 			result = SUCCESS;
-			if(search.equals("")){
-				resultSelect ="すべてを表示しました。";
-			}else{resultSelect = search + " を検索しました。";
+			if (search.equals("")) {
+				resultSelect = "すべてを表示しました。";
+			} else {
+				resultSelect = search + " を検索しました。";
 
 			}
 		}
 
+		int user_id = Integer.parseInt(search);
 		StudentsSelectDAO selectDao = new StudentsSelectDAO();
-		studentsList = selectDao.select("");
+		studentsList = selectDao.select(user_id);
 
 		return result;
 	}
+
 	/**
 	 * 取得メソッド 検索ワードを取得
+	 *
 	 * @author YUICHI KIRIU
 	 * @return search
 	 */
 	public String getSearch() {
 		return search;
 	}
+
 	/**
 	 * 設定メソッド 検索ワードを設定
+	 *
 	 * @author YUICHI KIRIU
 	 * @param search
 	 */
@@ -95,6 +101,7 @@ public class ProjectsSelectAction extends ActionSupport{
 
 	/**
 	 * 取得メソッド プロジェクトリストを取得
+	 *
 	 * @author YUICHI KIRIU
 	 * @return projectList
 	 */
@@ -104,6 +111,7 @@ public class ProjectsSelectAction extends ActionSupport{
 
 	/**
 	 * 設定メソッド プロジェクトリストを設定
+	 *
 	 * @author YUICHI KIRIU
 	 * @param projectList
 	 */
@@ -113,14 +121,17 @@ public class ProjectsSelectAction extends ActionSupport{
 
 	/**
 	 * 取得メソッド エラーメッセージを取得
+	 *
 	 * @author YUICHI KIRIU
 	 * @return resultString
 	 */
 	public String getErrorMsg() {
 		return resultSelect;
 	}
+
 	/**
 	 * 設定メソッド エラーメッセージを設定
+	 *
 	 * @author YUICHI KIRIU
 	 * @param resultString
 	 */
@@ -130,61 +141,72 @@ public class ProjectsSelectAction extends ActionSupport{
 
 	/**
 	 * 取得メソッド 検索結果文字を取得
+	 *
 	 * @author YUICHI KIRIU
 	 * @return resultString
 	 */
 	public String getResultSelect() {
 		return resultSelect;
 	}
+
 	/**
 	 * 設定メソッド 検索結果文字を設定
+	 *
 	 * @author YUICHI KIRIU
 	 * @param resultString
 	 */
 	public void setResultSelect(String resultSelect) {
 		this.resultSelect = resultSelect;
 	}
+
 	/**
 	 * 取得メソッド 検索リストを取得
+	 *
 	 * @author TATUHUMI ITOU
 	 * @return searchList
 	 */
 	public List<ProjectsSelectDTO> getSearchList() {
 		return searchList;
 	}
+
 	/**
 	 * 設定メソッド 検索リストを設定
+	 *
 	 * @author TATUHUMI ITOU
 	 * @param searchList
 	 */
 	public void setSearchList(List<ProjectsSelectDTO> searchList) {
 		this.searchList = searchList;
 	}
+
 	/**
-	* 取得メソッド 受講生リストを取得
-	* @author TATUHUMI ITOU
-	* @return studentsList
-	*/
+	 * 取得メソッド 受講生リストを取得
+	 *
+	 * @author TATUHUMI ITOU
+	 * @return studentsList
+	 */
 	public List<StudentsDTO> getStudentsList() {
 		return studentsList;
 	}
+
 	/**
-	* 設定メソッド 受講生リストを設定
-	* @author TATUHUMI ITOU
-	* @param studentsList
-	*/
+	 * 設定メソッド 受講生リストを設定
+	 *
+	 * @author TATUHUMI ITOU
+	 * @param studentsList
+	 */
 	public void setStudentsList(List<StudentsDTO> studentsList) {
 		this.studentsList = studentsList;
 	}
+
 	/**
-	* 取得メソッド シリアル番号を取得
-	* @author TATUHUMI ITOU
-	* @return serialVersionUID
-	*/
+	 * 取得メソッド シリアル番号を取得
+	 *
+	 * @author TATUHUMI ITOU
+	 * @return serialVersionUID
+	 */
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
 
-
 }
-
