b0e71d7f744038246b5695b94b5174e5a82012b3
yuri ota <hamakko825@ezweb.ne.jp>
2017-06-23 17:39:04 +0900

    AdminAttendanceDeleteActionの修正

--- src/com/internousdev/openconnect/attendance/action/AdminAttendanceDeleteAction.java
+++ src/com/internousdev/openconnect/attendance/action/AdminAttendanceDeleteAction.java
@@ -1,7 +1,9 @@
 /**
- * 
+ *
  */
 package com.internousdev.openconnect.attendance.action;
+
+import com.internousdev.openconnect.attendance.dao.AdminAttendanceDeleteDAO;
 
 /**
  * @author internousdev
@@ -9,4 +11,38 @@ package com.internousdev.openconnect.attendance.action;
  */
 public class AdminAttendanceDeleteAction {
 
+
+
+	private int atMonth;
+
+
+	public String execute(){
+		AdminAttendanceDeleteDAO dao = new AdminAttendanceDeleteDAO();
+		String retult = ERROR;
+		int List=0;
+		if(List>0){
+			retult = SUCCESS;
+		}
+		return retult;
+	}
+
+
+
+
+
+	/**
+	 * 取得メソッド を取得
+	 * @return atMonth
+	 */
+	public int getAtMonth() {
+		return atMonth;
+	}
+
+	/**
+	 * 設定メソッド を設定
+	 * @param atMonth
+	 */
+	public void setAtMonth(int atMonth) {
+		this.atMonth = atMonth;
+	}
 }
