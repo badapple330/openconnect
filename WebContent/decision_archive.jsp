50a1e6510517af557c0ba40a4b88e40de112878b
ShouheiKato <ktush9sc@gmail.com>
2017-06-29 18:13:20 +0900

    完了済み決済画面の作成

--- WebContent/decision_archive.jsp
+++ WebContent/decision_archive.jsp
@@ -1,12 +1,57 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<!-- Stylesheet
+      ================================================== -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="css/decision_archive.css">
+<!-- Javascripts
+      ================================================== -->
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+
+<title>完了済み決済</title>
+<script type="text/javascript">
+	//選択箇所全入力
+	$(function() {
+		$("#input").on("keyup change", function() {
+			$("#res,#lines,#c,#d").val($(this).val());
+		});
+	});
+</script>
+
+<jsp:include page="header.jsp" />
+
 </head>
 <body>
-完了済み決裁の一覧を表示予定
+	<div class="container">
+		<h2>完了済み決済一覧</h2>
+		<s:form action="DecisionArchiveAction">
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th>決済ID</th>
+						<th>プロジェクト名</th>
+						<th>プレビュー</th>
+				</thead>
+				<s:iterator value="archiveList">
+					<tbody>
+						<tr>
+							<td>
+							<s:property value="decisionId" />
+							</td>
+							<td>
+							<s:property value="decisionName" />
+							</td>
+							<td>プレビュー</td>
+						</tr>
+					</tbody>
+				</s:iterator>
+			</table>
+		</s:form>
+
+	</div>
 </body>
 </html>
--- src/com/internousdev/openconnect/decision/detail/action/DecisionArchiveAction.java
+++ src/com/internousdev/openconnect/decision/detail/action/DecisionArchiveAction.java
@@ -3,14 +3,31 @@
  */
 package com.internousdev.openconnect.decision.detail.action;
 
+import java.util.ArrayList;
+
+import com.internousdev.openconnect.decision.dto.DecisionDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class DecisionArchiveAction extends ActionSupport {
 
+	private static final long serialVersionUID = 6045130027645771884L; //シリアルID
+
 	/**
-	 *
+	 * 決済ID
 	 */
-	private static final long serialVersionUID = 6045130027645771884L;
+	private int decisionId;
+
+	/**
+	 * 案件名
+	 */
+	private String decisionName;
+
+	/**
+	 * 決裁手続きの情報をリスト化
+	 */
+	private ArrayList<DecisionDTO> archiveList = new  ArrayList<DecisionDTO>();
+
+
 
 	public String execute() {
 
@@ -21,5 +38,57 @@ public class DecisionArchiveAction extends ActionSupport {
 		}
 		return result;
 	}
+
+	/**
+	* 取得メソッド を取得
+	* @return decisionId
+	*/
+	public int getDecisionId() {
+		return decisionId;
+	}
+
+	/**
+	* 設定メソッド を設定
+	* @param decisionId
+	*/
+	public void setDecisionId(int decisionId) {
+		this.decisionId = decisionId;
+	}
+
+	/**
+	* 取得メソッド を取得
+	* @return decisionName
+	*/
+	public String getDecisionName() {
+		return decisionName;
+	}
+
+	/**
+	* 設定メソッド を設定
+	* @param decisionName
+	*/
+	public void setDecisionName(String decisionName) {
+		this.decisionName = decisionName;
+	}
+
+	/**
+	* 取得メソッド を取得
+	* @return archiveList
+	*/
+	public ArrayList<DecisionDTO> getArchiveList() {
+		return archiveList;
+	}
+
+	/**
+	* 設定メソッド を設定
+	* @param archiveList
+	*/
+	public void setArchiveList(ArrayList<DecisionDTO> archiveList) {
+		this.archiveList = archiveList;
+	}
+
+
+
+
 
 }
