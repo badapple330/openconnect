b6486493b53d04ca739f9ba29cd059f1d831afc5
Toshifumi Yamashita <yamashita_tf@yahoo.co.jp>
2017-06-29 09:50:31 +0900

    バグを修正

--- sql/20141001-ec10-phase1.sql
+++ sql/20141001-ec10-phase1.sql
@@ -17,12 +17,6 @@ order_destination varchar(255) not null comment '届け先',
 order_day datetime not null default current_timestamp comment '購入先'
 ) comment="注文情報テーブル";
 
-create table admin_table(
-admin_id varchar(16) not null primary key comment '管理者ID',
-admin_pass varchar(16) not null comment '管理者パスワード',
-admin_name varchar(50) not null comment '管理者名'
-) comment="管理者テーブル";
-insert into admin_table(admin_id,admin_pass,admin_name) values ("12345678","commando","ジョンメイトリックス");
 
 create table  user_table(
 user_id varchar(16) not null primary key comment 'ユーザーID',
