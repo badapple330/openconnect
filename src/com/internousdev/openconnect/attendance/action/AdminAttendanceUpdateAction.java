4173948795e524931fd908757465b17095a61e7c
Konosuke Kawamura <gtio_rgoi.kmii-kroara@ezweb.ne.jp>
2017-07-12 15:50:54 +0900

    AdminAttendanceUpdateActionとDAOの修正

--- src/com/internousdev/openconnect/attendance/action/AdminAttendanceUpdateAction.java
+++ src/com/internousdev/openconnect/attendance/action/AdminAttendanceUpdateAction.java
@@ -4,15 +4,20 @@
 package com.internousdev.openconnect.attendance.action;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
 
 import com.internousdev.openconnect.attendance.dao.AdminAttendanceUpdateDAO;
+import com.internousdev.openconnect.attendance.dto.AttendanceDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
  * @author internousdev
  *
  */
-public class AdminAttendanceUpdateAction extends ActionSupport{
+public class AdminAttendanceUpdateAction extends ActionSupport implements SessionAware{
 
 	/**
      * シリアル番号
@@ -31,6 +36,20 @@ public class AdminAttendanceUpdateAction extends ActionSupport{
     private int atMonth;
 //    日
     private int atDay;
+//    チーム名
+    private String teamName;
+//    日付
+    private String atDate;
+//    受講開始月
+    private String month;
+//    漢字姓
+    private String familyNameKanji;
+//    漢字名
+    private String givenNameKanji;
+
+
+    /* 検索結果 */
+    ArrayList<AttendanceDTO> searchList = new ArrayList<AttendanceDTO>();
 
     public String execute() throws SQLException {
 
@@ -38,7 +57,9 @@ public class AdminAttendanceUpdateAction extends ActionSupport{
         AdminAttendanceUpdateDAO dao = new AdminAttendanceUpdateDAO();
         int count = 0;
         count = dao.update(userId,attendance,reason,atYear,atMonth,atDay);
+
         if (count != 0) {
+        	searchList = dao.select(atYear,atMonth,atDay,teamName);
             result = SUCCESS;
 
         }
@@ -47,100 +68,228 @@ public class AdminAttendanceUpdateAction extends ActionSupport{
 
 
 	/**
-	* 取得メソッド を取得
-	* @return userId
-	*/
+	 * userIdを取得します。
+	 * @return userId
+	 */
 	public int getUserId() {
 		return userId;
 	}
+
+
 	/**
-	* 設定メソッド を設定
-	* @param userId
-	*/
+	 * userIdを設定します。
+	 * @param userId userId
+	 */
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+
+
 	/**
-	* 取得メソッド を取得
-	* @return attendance
-	*/
+	 * attendanceを取得します。
+	 * @return attendance
+	 */
 	public String getAttendance() {
 		return attendance;
 	}
+
+
 	/**
-	* 設定メソッド を設定
-	* @param attendance
-	*/
+	 * attendanceを設定します。
+	 * @param attendance attendance
+	 */
 	public void setAttendance(String attendance) {
 		this.attendance = attendance;
 	}
+
+
 	/**
-	* 取得メソッド を取得
-	* @return reason
-	*/
+	 * reasonを取得します。
+	 * @return reason
+	 */
 	public String getReason() {
 		return reason;
 	}
+
+
 	/**
-	* 設定メソッド を設定
-	* @param reason
-	*/
+	 * reasonを設定します。
+	 * @param reason reason
+	 */
 	public void setReason(String reason) {
 		this.reason = reason;
 	}
 
 
 	/**
-	* 取得メソッド を取得
-	* @return atYear
-	*/
+	 * atYearを取得します。
+	 * @return atYear
+	 */
 	public int getAtYear() {
 		return atYear;
 	}
 
 
 	/**
-	* 設定メソッド を設定
-	* @param atYear
-	*/
+	 * atYearを設定します。
+	 * @param atYear atYear
+	 */
 	public void setAtYear(int atYear) {
 		this.atYear = atYear;
 	}
 
 
 	/**
-	* 取得メソッド を取得
-	* @return atMonth
-	*/
+	 * atMonthを取得します。
+	 * @return atMonth
+	 */
 	public int getAtMonth() {
 		return atMonth;
 	}
 
 
 	/**
-	* 設定メソッド を設定
-	* @param atMonth
-	*/
+	 * atMonthを設定します。
+	 * @param atMonth atMonth
+	 */
 	public void setAtMonth(int atMonth) {
 		this.atMonth = atMonth;
 	}
 
 
 	/**
-	* 取得メソッド を取得
-	* @return atDay
-	*/
+	 * atDayを取得します。
+	 * @return atDay
+	 */
 	public int getAtDay() {
 		return atDay;
 	}
 
 
 	/**
-	* 設定メソッド を設定
-	* @param atDay
-	*/
+	 * atDayを設定します。
+	 * @param atDay atDay
+	 */
 	public void setAtDay(int atDay) {
 		this.atDay = atDay;
+	}
+
+
+	/**
+	 * teamNameを取得します。
+	 * @return teamName
+	 */
+	public String getTeamName() {
+		return teamName;
+	}
+
+
+	/**
+	 * teamNameを設定します。
+	 * @param teamName teamName
+	 */
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+
+	/**
+	 * atDateを取得します。
+	 * @return atDate
+	 */
+	public String getAtDate() {
+	    return atDate;
+	}
+
+
+	/**
+	 * atDateを設定します。
+	 * @param atDate atDate
+	 */
+	public void setAtDate(String atDate) {
+	    this.atDate = atDate;
+	}
+
+
+	/**
+	 * monthを取得します。
+	 * @return month
+	 */
+	public String getMonth() {
+	    return month;
+	}
+
+
+	/**
+	 * monthを設定します。
+	 * @param month month
+	 */
+	public void setMonth(String month) {
+	    this.month = month;
+	}
+
+
+	/**
+	 * familyNameKanjiを取得します。
+	 * @return familyNameKanji
+	 */
+	public String getFamilyNameKanji() {
+	    return familyNameKanji;
+	}
+
+
+	/**
+	 * familyNameKanjiを設定します。
+	 * @param familyNameKanji familyNameKanji
+	 */
+	public void setFamilyNameKanji(String familyNameKanji) {
+	    this.familyNameKanji = familyNameKanji;
+	}
+
+
+	/**
+	 * givenNameKanjiを取得します。
+	 * @return givenNameKanji
+	 */
+	public String getGivenNameKanji() {
+	    return givenNameKanji;
+	}
+
+
+	/**
+	 * givenNameKanjiを設定します。
+	 * @param givenNameKanji givenNameKanji
+	 */
+	public void setGivenNameKanji(String givenNameKanji) {
+	    this.givenNameKanji = givenNameKanji;
+	}
+
+
+	/**
+	 * searchListを取得します。
+	 * @return searchList
+	 */
+	public ArrayList<AttendanceDTO> getSearchList() {
+	    return searchList;
+	}
+
+
+	/**
+	 * searchListを設定します。
+	 * @param searchList searchList
+	 */
+	public void setSearchList(ArrayList<AttendanceDTO> searchList) {
+	    this.searchList = searchList;
+	}
+
+
+	/* (非 Javadoc)
+	 * @see org.apache.struts2.interceptor.SessionAware#setSession(java.util.Map)
+	 */
+	@Override
+	public void setSession(Map<String, Object> arg0) {
+		// TODO 自動生成されたメソッド・スタブ
+
 	}
 
 }
--- src/com/internousdev/openconnect/attendance/dao/AdminAttendanceUpdateDAO.java
+++ src/com/internousdev/openconnect/attendance/dao/AdminAttendanceUpdateDAO.java
@@ -5,18 +5,33 @@ package com.internousdev.openconnect.attendance.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+import com.internousdev.openconnect.attendance.dto.AttendanceDTO;
 import com.internousdev.util.DBConnector;
 
 /**
- * @author internousdev
- *
+ * 検索情報を編集するクラス
+ * @author kawamura
+ * @since 7/11
+ * @version 1.0.0
  */
 public class AdminAttendanceUpdateDAO {
 
+	/**
+	 * 検索情報をもとに勤怠と備考を編集するメソッド
+	 * @param userId
+	 * @param attendance
+	 * @param reason
+	 * @param atYear
+	 * @param atMonth
+	 * @param atDay
+	 * @return result
+	 */
 	 public int update(int userId,String attendance,String reason,int atYear,int atMonth,int atDay) {
-
 
 	        DBConnector db = new DBConnector("com.mysql.jdbc.Driver","jdbc:mysql://localhost/", "openconnect","root","mysql");
 	        Connection con = db.getConnection();
@@ -47,6 +62,57 @@ public class AdminAttendanceUpdateDAO {
 
 	        return result;
 	    }
+	 /**
+	  * 編集を実行した時に検索もする為のメソッド
+	  * @param atYear
+	  * @param atMonth
+	  * @param atDay
+	  * @param teamName
+	  * @return searchList
+	  */
+	 public ArrayList<AttendanceDTO> select(int atYear, int atMonth, int atDay, String teamName){
+		 DBConnector db = new DBConnector("com.mysql.jdbc.Driver","jdbc:mysql://localhost/", "openconnect","root","mysql");
+		 Connection con = db.getConnection();
+		 Statement statement = null;
+
+		 /* 検索結果格納DTO */
+		 ArrayList<AttendanceDTO> searchList = new ArrayList<AttendanceDTO>();
+
+		 String sql = "select * from attendance left join users on attendance.user_id=users.user_id where at_year="+ atYear+" and at_month="+ atMonth+" and at_day="+ atDay+" and team_name='"+ teamName+"'";
+
+		 try {
+			 statement = con.createStatement();
+			 ResultSet rs = statement.executeQuery(sql);
+
+			 while (rs.next()) {
+				 AttendanceDTO dto = new AttendanceDTO();
+				 dto.setAtDate(rs.getString("at_date"));
+					dto.setMonth(rs.getString("month"));
+					dto.setFamilyNameKanji(rs.getString("family_name_kanji"));
+					dto.setGivenNameKanji(rs.getString("given_name_kanji"));
+					dto.setTeamName(rs.getString("team_name"));
+					dto.setAttendance(rs.getString("attendance"));
+					dto.setReason(rs.getString("reason"));
+					dto.setUserId(rs.getInt("user_Id"));
+					dto.setAtYear(rs.getInt("at_year"));
+					dto.setAtMonth(rs.getInt("at_month"));
+					dto.setAtDay(rs.getInt("at_day"));
+					dto.setUserName(rs.getString("user_name"));
+					searchList.add(dto);
+			 }
+
+		 } catch (SQLException e) {
+			 e.printStackTrace();
+		 } finally {
+			 try {
+				 con.close();
+				 statement.close();
+			 } catch (SQLException e) {
+				 e.printStackTrace();
+			 }
+		 }
+		 return searchList;
+	 }
 
 	}
 
